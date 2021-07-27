5f7f4361b4aec2b1643ed3dc4c0ccf81c61a815f
Yutaka Fujii <yutakaf@playwell.site>
2021-07-27 23:44:12 +0900

    s3 setting added

--- lib/first-aws-cdk-stack.ts
+++ lib/first-aws-cdk-stack.ts
@@ -1,9 +1,26 @@
-import * as cdk from '@aws-cdk/core';
+import * as cdk from '@aws-cdk/core'
+import { Bucket } from '@aws-cdk/aws-s3'
+import { CfnOutput, CfnParameter, Duration } from '@aws-cdk/core'
 
 export class FirstAwsCdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    super(scope, id, props)
+    const duration = new CfnParameter(this, 'duration', {
+      type: 'Number',
+      default: 6,
+      minValue: 1,
+      maxValue: 10,
+    })
 
-    // The code that defines your stack goes here
+    const myBucket = new Bucket(this, 'MyFirstBucket', {
+      lifecycleRules: [
+        {
+          expiration: Duration.days(duration.valueAsNumber),
+        },
+      ],
+    })
+    new CfnOutput(this, 'myBucket', {
+      value: myBucket.bucketName,
+    })
   }
 }
